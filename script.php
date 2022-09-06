6275ddc88adfa3fc30b4d8a1bbbaaf4168c6a1e4
Ray1024 <r.1024@yandex.ru>
2022-09-06 12:58:54 +0300

    5

--- script.php
+++ script.php
@@ -1,8 +1,5 @@
 <?php
 
-$filename = __DIR__ . '/page.html';
-$document = new DOMDocument();
-$document->loadHTMLFile($filename);
 
 function check($x, $y, $r)
 {
@@ -17,20 +14,101 @@ function check($x, $y, $r)
     return false;
 }
 
-if (isset($_POST["xRadio"]) && isset($_POST["yText"]) && isset($_POST["rCheckbox"]) && count($_POST["rCheckbox"]) == 1) {
-    $xRadio = floatval($_POST["xRadio"]);
-    $yText = floatval($_POST["yText"]);
-    $rCheckbox = null;
-    foreach ($_POST["rCheckbox"] as $i) {
-        $rCheckbox = floatval($i);
-    }
 
-    echo '( ' . $xRadio . ' , ' . $yText . ' ) Radius: ' . $rCheckbox . PHP_EOL;
-    if (check($xRadio, $yText, $rCheckbox)) {
-        echo 'YES';
-    } else echo 'NO';
+function loadResultsArray($filename)
+{
+    /*$filename = __DIR__ . '/results.txt';
+    $text = file_get_contents($filename);
+    if ($text == false) {
+        return array();
+    } else {
+        $lines = preg_split("\n", $text);
+
+        if ($lines == null) return array();
+        for ($i = 0; $i < count($lines); $i++) {
+            $lines[i] = preg_split(" ", $lines[i]);
+            if ($lines[i] == false) return array();
+        }
+        return $lines;
+    }*/
+    $text = file_get_contents($filename);
+    if ($text == false) return array();
+    $array = unserialize(base64_decode($text));
+    if ($array == false) return array();
+    return $array;
 }
 
 
-echo $document->saveHTML();
+function saveResultsArray($array, $filename)
+{
+    $text = base64_encode(serialize($array));
+    file_put_contents($filename, $text);
+}
+
+
+function fillResultsTable($document, $resultsArray)
+{
+    if ($document == null) return;
+    $table = $document->getElementById("resultTable");
+    if ($table == null) return;
+
+    for ($i = 0; $i < count($resultsArray); ++$i) {
+        ///////////////////////////////////////////////////////////////////////////////////////
+        $newLine = new DOMElement("tr");
+        $cellNumber = new DOMElement("td");
+        $cellDateTime = new DOMElement("td");
+        $cellX = new DOMElement("td");
+        $cellY = new DOMElement("td");
+        $cellR = new DOMElement("td");
+        $cellResponse = new DOMElement("td");
+        ///////////////////////////////////////////////////////////////////////////////////////
+        $table->appendChild($newLine);
+        $newLine->appendChild($cellNumber);
+        $newLine->appendChild($cellDateTime);
+        $newLine->appendChild($cellX);
+        $newLine->appendChild($cellY);
+        $newLine->appendChild($cellR);
+        $newLine->appendChild($cellResponse);
+        //////////////////////////////////////////////////////////////////////////////////////
+        $cellNumber->textContent = "6";
+        $cellDateTime->textContent = "5";
+        $cellX->textContent = "4";
+        $cellY->textContent = "3";
+        $cellR->textContent = "2";
+        $cellResponse->textContent = "1";
+    }
+}
+
+function func()
+{
+    $filename = __DIR__ . '/page.html';
+    $document = new DOMDocument();
+    $document->loadHTMLFile($filename);
+
+
+    if (isset($_POST["xRadio"]) && isset($_POST["yText"]) && isset($_POST["rCheckbox"]) && count($_POST["rCheckbox"]) == 1) {
+
+        $xRadio = floatval($_POST["xRadio"]);
+        $yText = floatval($_POST["yText"]);
+        $rCheckbox = null;
+
+        foreach ($_POST["rCheckbox"] as $i) {
+            $rCheckbox = floatval($i);
+        }
+
+        /*echo '( ' . $xRadio . ' , ' . $yText . ' ) Radius: ' . $rCheckbox . PHP_EOL;
+        if (check($xRadio, $yText, $rCheckbox)) {
+            echo 'YES';
+        } else echo 'NO';*/
+
+        $arr = loadResultsArray($filename);
+        fillResultsTable($document, $arr);
+        saveResultsArray($arr, $filename);
+    }
+
+    echo $document->saveHTML();
+}
+
+func();
+
 ?>
